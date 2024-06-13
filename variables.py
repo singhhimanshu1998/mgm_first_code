2f9e3cec14876094604c54a7921d17e10bb14c0e
singhhimanshu1998 <singh.himanshu123.hs@gmail.com>
2024-06-13 14:57:58 +0530

    Your commit message

---
+++ variables.py
@@ -0,0 +1,15 @@
+import random
+
+randNumber = str(random.randrange(1000, 9999))
+randomNumber = str(random.randrange(10000, 99999))
+
+email = "automation@practicallogix.com"
+password = "ATBy6E#K7AH&axL5@"
+automation_listt = "Automation List" + randNumber
+new_List_name = "automation" + randNumber
+mailinator_email = "automation" + randNumber + "@mailinator.com"
+mailinator_2email = "automation" + randomNumber + "@mailinator.com"
+delete_listtt = "autoDelete" + randNumber
+mailinator_email1 = "demo" + randNumber + "@mailinator.com"
+mailinator_email2 = "test" + randNumber + "@mailinator.com"
+email_address = "automation@practicallogix.com"
